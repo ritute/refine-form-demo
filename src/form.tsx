6c136cea0f0abfc664508f422cc35cc1edbcf855
ritute <rita@ritute.com>
2024-07-18 12:21:29 -0400

    fix(form): syntax error

--- src/form.tsx
+++ src/form.tsx
@@ -21,64 +21,62 @@ export const PostEdit: React.FC = () => {
 	})
 
 	return (
-		// <Edit saveButtonProps={saveButtonProps}>
-			<Box component="form" sx={{ display: 'flex', flexDirection: 'column' }} autoComplete="off">
-				<TextField
-					id="title"
-					{...register('title', {
-						required: 'This field is required',
-					})}
-					error={!!errors.title}
-					helperText={errors.title?.message}
-					margin="normal"
-					fullWidth
-					label="Title"
-					name="title"
-					autoFocus
-				/>
+		<Box component="form" sx={{ display: 'flex', flexDirection: 'column' }} autoComplete="off">
+			<TextField
+				id="title"
+				{...register('title', {
+					required: 'This field is required',
+				})}
+				error={!!errors.title}
+				helperText={errors.title?.message}
+				margin="normal"
+				fullWidth
+				label="Title"
+				name="title"
+				autoFocus
+			/>
 
-				<Controller
-					control={control}
-					name="status"
-					rules={{ required: 'This field is required' }}
-					defaultValue={null as any}
-					render={({ field }) => (
-						<Autocomplete<IStatus>
-							id="status"
-							options={['published', 'draft', 'rejected']}
-							{...field}
-							onChange={(_, value) => {
-								field.onChange(value)
-							}}
-							renderInput={(params) => (
-								<TextField
-									{...params}
-									label="Status"
-									margin="normal"
-									variant="outlined"
-									error={!!errors.status}
-									helperText={errors.status?.message}
-									required
-								/>
-							)}
-						/>
-					)}
-				/>
+			<Controller
+				control={control}
+				name="status"
+				rules={{ required: 'This field is required' }}
+				defaultValue={null as any}
+				render={({ field }) => (
+					<Autocomplete<IStatus>
+						id="status"
+						options={['published', 'draft', 'rejected']}
+						{...field}
+						onChange={(_, value) => {
+							field.onChange(value)
+						}}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								label="Status"
+								margin="normal"
+								variant="outlined"
+								error={!!errors.status}
+								helperText={errors.status?.message}
+								required
+							/>
+						)}
+					/>
+				)}
+			/>
 
-				<TextField
-					id="content"
-					{...register('content', {
-						required: 'This field is required',
-					})}
-					error={!!errors.content}
-					helperText={errors.content?.message}
-					margin="normal"
-					label="Content"
-					multiline
-					rows={4}
-				/>
-			</Box>
-		{/* </Edit> */}
+			<TextField
+				id="content"
+				{...register('content', {
+					required: 'This field is required',
+				})}
+				error={!!errors.content}
+				helperText={errors.content?.message}
+				margin="normal"
+				label="Content"
+				multiline
+				rows={4}
+			/>
+		</Box>
 	)
 }
 
